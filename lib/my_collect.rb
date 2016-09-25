4c61457102767dbbdba45af79964db7a65170f7c
Yardagan AL Hamed <gooryalhamed@hotmail.com>
2016-09-25 10:36:07 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
+def my_collect(collection)
+  i =0
+  firstnames =[]
+  while i < collection.length
+    firstnames << yield(collection[i])
+    i += 1
+  end
+  firstnames
+end
 
-
+collection =["goory alhamed","audrey watne"]
+my_collect(collection) do |name|
+  name.split(" ").first
+end
