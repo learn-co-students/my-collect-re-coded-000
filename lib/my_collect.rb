690990315bbb7f0adb92b0cb119f9fc960c820ea
Ahmed Mohammed <iahmed215@gmail.com>
2016-10-19 17:44:55 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
-
-
+def my_collect(lang)
+cap=[]
+i=0
+while i<lang.length
+  cap<<yield(lang[i])
+i+=1
+end
+cap
+end
+lang=["ruby", "javascript", "python", "objective-c"]
+my_collect(lang) { |name| name.upcase }
