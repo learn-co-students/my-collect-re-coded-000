a9a1154346e53c2c1d3547a0a6146f2365c00f35
nichervan brhik <nichervan.brhik@gmail.com>
2017-01-08 11:12:28 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,18 @@
-
-
+def my_collect(collection)
+  x = 0
+  collect=[]
+  while x < collection.length
+    collect << yield(collection[x])
+        x+=1
+  end
+  collect
+end
+# def my_collect(array)
+#   i = 0
+#   collect = []
+#   while i < array.length
+#     collect << yield(array[i])
+#     i+=1
+#   end
+#   collect
+# end
