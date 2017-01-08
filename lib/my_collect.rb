5b4ceee2392fac49014d964a290b4aaf313548ce
rezanrezano <captenrez@gmail.com>
2017-01-08 22:03:56 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,10 @@
-
-
+def my_collect(array)#defin the method
+  counter=0
+  collection=[]                        #firstly the collection is empty
+  element=array.count
+  while counter<element
+     collection<<yield(array[counter])        #apply yield and get a new collection
+     counter+=1
+   end
+  return collection
+ end
